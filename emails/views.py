589f0c1274afb2c8ab2274bfae0e36cf9c70ca51
Devin Rowland <devinrowland12@gmail.com>
2020-08-17 16:50:52 -0700

    setting up basic template form

--- emails/views.py
+++ emails/views.py
@@ -5,13 +5,18 @@ from django.http import HttpResponse, Http404
 
 from .models import EmailEntry
 
+html_str = "<!doctype html><html><body><h1>{email}</h1></body></html>"
+
 def email_entry_get_view(request, id=None, *args, **kwargs):
     #get a single item stored in the database
     try:
         obj = EmailEntry.objects.get(id=id)
     except EmailEntry.DoesNotExist:
         raise Http404
-    return HttpResponse(f"<h1>Hello {obj.email}</h1>")
+    #my_html = html.str.format(email=obj.email)
+    #return HttpResponse(f"<h1>Hello {obj.email}</h1>")
+    return render(request, "get.html", 
+    {"object": obj, "email": "abc@gmail.com"})
 
 #def email_entry_list_view():
 
