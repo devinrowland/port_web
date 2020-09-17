3121a97aa3e01e03945359ec281b3fc0a7d56066
Devin Rowland <devinrowland12@gmail.com>
2020-09-17 15:05:44 -0700

    creating register form

--- accounts/views.py
+++ accounts/views.py
@@ -13,7 +13,7 @@ def logout_view(request):
 
 def login_view(request):
     form = LoginForm(request.POST or None)
-    if form.isvalid():
+    if form.is_valid():
         username = form.cleaned_data.get("username")
         password = form.cleaned_data.get("password")
 
@@ -26,10 +26,15 @@ def login_view(request):
         login(request, user)
         #redirect
         return redirect("/")
-
-
     return render(request, "accounts/login.html",{"form":form})
 
 def register_view(request):
     form = RegisterForm(request.POST or None)
-    return render(request, "accounts/register.html", {})
+    if form.is_valid():
+        user_obj = form.save(commit=False)
+        password = form.cleaned_data.get("password")
+        user_obj.save()
+        user_obj.set_password(password)
+        user_obj.save()
+        return redirect("/login")
+    return render(request, "accounts/register.html", {"form": form})
